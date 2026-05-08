4381b01776c3550363a457df42c519a5f3d1203c
Emi <egomezdevita@gmail.com>
2026-05-08 17:48:32 -0300

    Configurando con servidor en express

---
+++ Backend/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const app = express();
+
+/*
+✔ require() → importar
+✔ express() → crear app
+✔ app.get() → rutas GET
+✔ res.send() → responder
+✔ app.listen() → abrir servido
+*/
+
+///ESTRUCTURA DEL .get("ruta", function(request o response))/ este sirve para "PEDIR"
+// app.get("/", function(req, res){
+//     res.send("Bienvenido al servidor")
+// });
+
+app.listen(3000, function(){
+    console.log("Servidor corriendo")
+});
+
+app.use(express.json())
+///NOTAS: no es ecesario que la ruta("/") tenga el mismo nombre que el HTML
+///el req.body abarca todo como u obj, si quiero especificar en algo uso req.body.datoEspecifico(ej:nombre ,numero, etc)
+app.post("/pedidos", function(req, res){
+    console.log(req.body)
+    res.send("Data received")
+});
