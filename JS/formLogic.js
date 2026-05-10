d753d270bf7f134392088647f931b65a86802275
Emi <egomezdevita@gmail.com>
2026-05-10 10:38:13 -0300

    mejora en el funcionamiento del evento submit del formulario, agregando obj pedido

--- JS/formLogic.js
+++ JS/formLogic.js
@@ -2,52 +2,48 @@ console.log("JS conectado");
 document.addEventListener("DOMContentLoaded", function () {
   ///LLAMANDO A LOS ATRIBUTOS DEL FORM
   const alertaEnviar = document.getElementById("toastForm");
-  const nombreForm = document.getElementById("nombre").value;
-  const emailForm = document.getElementById("email").value;
-  const servicioForm = document.getElementById("servicio").value;
-  const descripcionForm = document.getElementById("descripcion").value;
-  ///creando el obj con los atrbutos del form
-  const pedido = {
-    Nombre: nombreForm,
-    Email: emailForm,
-    Servicio: servicioForm,
-    Descrpcion: descripcionForm,
-  };
-
-  console.log(pedido);
-
-  //Mejoras en el formulario de servicios.html
   const formulario = document.getElementById("formPedido");
+  //Mejoras en el formulario de servicios.html
 
-  formulario.addEventListener("submit", function (event) {
-    event.preventDefault();
+    if(formulario){
+        formulario.addEventListener("submit", function (event) {
+            event.preventDefault();
 
-    fetch("http://localhost:3000/pedidos", {
-      method: "POST",
-      body: JSON.stringify(pedido),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => {
-        if (response.ok) {
-          ///alert("¡Gracias por su pedido! Su formulario ha sido enviado con éxito.");
-          alertaEnviar.classList.remove("show");
-          void alertaEnviar.offsetWidth; // Truco para resetear animaciones CSS
-          alertaEnviar.classList.add("show");
-        } else {
-          // El servidor respondió con un error (e.g., 404, 500)
-          alert(
-            "Hubo un problema al enviar su pedido. Por favor, inténtelo de nuevo más tarde.",
-          );
-        }
-      })
-      .catch((error) => {
-        // Error de red o algo impidió que la petición se completara
-        console.error("Error en la petición fetch:", error);
-        alert(
-          "Hubo un error de conexión al enviar su pedido. Por favor, revise su conexión a internet.",
-        );
-      });
-  });
+            const pedido =  {
+                Nombre: document.getElementById("nombre").value,
+                Email: document.getElementById("email").value,
+                Servicio: document.getElementById("servicio").value,
+                Descripcion: document.getElementById("descripcion").value
+
+            }
+            fetch("http://localhost:3000/pedidos", {
+            method: "POST",
+            body: JSON.stringify(pedido),
+            headers: {
+                "Content-Type": "application/json",
+            },            
+            })
+            .then((response) => {
+                if (response.ok) {
+                    ///alert("¡Gracias por su pedido! Su formulario ha sido enviado con éxito.");
+                    alertaEnviar.classList.remove("show");
+                    void alertaEnviar.offsetWidth; // Truco para resetear animaciones CSS
+                    alertaEnviar.classList.add("show");
+
+                } else {
+                alert(
+                    "Hubo un problema al enviar su pedido. Por favor, inténtelo de nuevo más tarde.",
+                );
+                }
+            })
+            .catch((error) => {
+                // Error de red o algo impidió que la petición se completara
+                console.error("Error en la petición fetch:", error);
+                alert(
+                "Hubo un error de conexión al enviar su pedido. Por favor, revise su conexión a internet.",
+                );
+            });
+        }); 
+    }
+  
 });
