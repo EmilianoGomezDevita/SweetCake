235849635a30fabce3ced3c9e0a11c7e46c26731
Emi <egomezdevita@gmail.com>
2026-05-11 09:27:33 -0300

    Mejora en el archivo pedido-controllers.js para que los pedidos se guarden en la DB del sitio(SweetCakeDB)

--- Backend/controllers/pedido-controllers.js
+++ Backend/controllers/pedido-controllers.js
@@ -1,18 +1,28 @@
 const { json } = require("express");
-const {mssql} = require("../config/db")
+const {mssql, conectarDB} = require("../config/db");
 
 const crearPedido = async (req, res) => {
     try{
-        console.log("Datos recibidos en el ontrolador: ", req.body)
-        // Aquí es donde luego haremos el pool.request().query(...)
+        //
+       const {cliente, producto, cantidad} = req.body
+       //OBTNEMOS EL POOL DE CONEXION
+       const pool = await conectarDB()
+
+       await pool.request()
+            .input("cliente", mssql.VarChar, cliente)
+            .input("producto", mssql.VarChar, producto)
+            .input("cantidad", mssql.Int, cantidad)
+            .query("INSERT into Pedidos(cliente, producto, cantidad) VALUES (@cliente, @producto, @cantidad)")
         // Por ahora, solo confirmamos recepción
-        res.status(201),json({
-            mensaje: "Pedido recibido correctamente",
+        res.status(201).json({
+            mensaje: "Pedido guardado exitosamente en SweetCakeDB",
             data: req.body
         })
     }
     catch(error){
-        res.status(500).json({mensaje: "Error al procesar el pedido: ", error})
+        console.error("Error en el INSERT:", error)
+
+        res.status(500).json({mensaje: "Error al guarada en DB: ", error: error.message})
 
     }
 };
