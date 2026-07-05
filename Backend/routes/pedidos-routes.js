52c53085fc8742814c3539517729414615bb36e9
Emiliano <egomezdevita@gmail.com>
2026-07-05 10:15:29 -0300

    Remove unused routes and mailer utility; update product button logic and form submission handling; enhance database schema for orders.

--- Backend/routes/pedidos-routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const {crearPedido} = require("../controllers/pedido-controllers")
-
-///NOTAS: no es ecesario que la ruta("/") tenga el mismo nombre que el HTML
-///el req.body abarca todo como u obj, si quiero especificar en algo uso req.body.datoEspecifico(ej:nombre ,numero, etc)
-router.post("/pedidos", crearPedido)
-
-module.exports = router
